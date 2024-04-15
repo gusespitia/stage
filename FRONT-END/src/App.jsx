d6ebb856eaa9cae35f38348f9d0e69aa3240f7e0
Gustavo E. <gustavespitia@gmail.com>
2024-04-15 14:51:02 +0200

    components divided

--- FRONT-END/src/App.jsx
+++ FRONT-END/src/App.jsx
@@ -3,18 +3,17 @@ import {
   Route,
   Link,
   Routes,
-  useNavigate,
 } from "react-router-dom";
 
-import { useState, useEffect } from "react"; // Agrega useState y useEffect
-import Userfront, {
-  SignupForm,
-  LoginForm,
-  PasswordResetForm,
-  LogoutButton,
-} from "@userfront/toolkit/react";
+
+import Userfront from "@userfront/toolkit/react";
 
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Reset from "./components/Reset";
+import Register from "./components/Register";
 
 Userfront.init("xbpwd96n");
 
@@ -52,9 +51,8 @@ function App() {
       </nav>
 
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={< Dashboard />} />
         <Route path="/reset" element={<Reset />} />
-
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
@@ -62,165 +60,4 @@ function App() {
     </Router>
   );
 }
-
-const Dashboard = () => {
-  const [privateData, setPrivateData] = useState(null); // Inicializa privateData como null
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3010/data", {
-          headers: {
-            Authorization: `Bearer ${Userfront.accessToken()}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const result = await response.json();
-        setPrivateData(result.someSecretData); // Extrae la data secreta del resultado
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  // REDIRECCIONAR SI NO ESTA LOGEADO
-  const navigate = useNavigate();
-
-  if (!Userfront.accessToken()) {
-    navigate("/login");
-    return null; // o puedes retornar algo mientras se realiza la redirección
-  }
-
-  // MOSTRAR LA INFO DEL USER
-  console.log(Userfront);
-  const userData = JSON.parse(atob(Userfront.accessToken().split('.')[1]));
-
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <h3>User Data</h3>
-      <pre>{JSON.stringify(userData, null, 2)}</pre>
-      <pre>{JSON.stringify(privateData, null, 2)}</pre>
-       <LogoutButton
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    </div>
-  );
-};
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-    <SignupForm
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    <LogoutButton
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    ;
-  </div>
-);
-const Login = () => (
-  <div>
-    <h2>Login</h2>
-    <LoginForm
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    ;
-  </div>
-);
-const Register = () => (
-  <div>
-    <h2>Register</h2>
-    <SignupForm
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    ;
-  </div>
-);
-
-const Reset = () => (
-  <div>
-    <h2>Reset</h2>
-    <PasswordResetForm
-      theme={{
-        colors: {
-          light: "#ffffff",
-          dark: "#5e72e4",
-          accent: "#13a0ff",
-          lightBackground: "#fdfdfd",
-          darkBackground: "#2d2d2d",
-        },
-        colorScheme: "light",
-        fontFamily: "Avenir, Helvetica, Arial, sans-serif",
-        size: "compact",
-        extras: { rounded: true, hideSecuredMessage: false },
-      }}
-    />
-    ; ;
-  </div>
-);
-
 export default App;
